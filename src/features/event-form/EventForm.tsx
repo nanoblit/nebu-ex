c88ab5d06092b42e895879b32acc2404bfc0d5d8
nanoblit <kuba9519@gmail.com>
2021-09-23 23:35:53 +0200

    Reset eventData after adding Event.

--- src/features/event-form/EventForm.tsx
+++ src/features/event-form/EventForm.tsx
@@ -38,31 +38,31 @@ const EventForm: React.FC = () => {
   ) => {
     switch (fieldName) {
       case "firstName":
-        const firstNameValid = value.match(/^[a-zA-Z ]{2,30}$/);
-        if (!firstNameValid) {
+        const isFirstNameValid = value.match(/^[a-zA-Z ]{2,30}$/);
+        if (!isFirstNameValid) {
           errorList.push("Please enter a valid first name");
         }
         break;
       case "lastName":
-        const lastNameValid = value.match(/^[a-zA-Z ]{2,30}$/);
-        if (!lastNameValid) {
+        const isLastNameValid = value.match(/^[a-zA-Z ]{2,30}$/);
+        if (!isLastNameValid) {
           errorList.push("Please enter a valid last name");
         }
         break;
       case "email":
-        const emailValid = value.toLowerCase().match(
+        const isEmailValid = value.toLowerCase().match(
           // eslint-disable-next-line no-useless-escape
           /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
         );
-        if (!emailValid) {
+        if (!isEmailValid) {
           errorList.push("Please enter a valid email");
         }
         break;
       case "eventDate":
         const date = new Date(value).getTime();
         const currentDate = Date.now();
-        const dateValid = date - currentDate > 0;
-        if (!dateValid) {
+        const isDateValid = date - currentDate > 0;
+        if (!isDateValid) {
           errorList.push("Please enter a date after today");
         }
         break;
@@ -73,7 +73,16 @@ const EventForm: React.FC = () => {
 
   const showAndHideSubmitMessage = () => {
     setIsSubmitMessageShown(true);
-    setTimeout(() => setIsSubmitMessageShown(false), 2000);
+    setTimeout(() => setIsSubmitMessageShown(false), 5000);
+  };
+
+  const resetEventData = () => {
+    setEventData({
+      firstName: "",
+      lastName: "",
+      email: "",
+      eventDate: format(Date.now(), "yyyy-MM-dd"),
+    });
   };
 
   const submitEvent = (e: React.FormEvent<HTMLFormElement>) => {
@@ -86,6 +95,7 @@ const EventForm: React.FC = () => {
     if (errorList.length === 0) {
       showAndHideSubmitMessage();
       dispatch(addEvent(eventData));
+      resetEventData();
     }
   };
 
